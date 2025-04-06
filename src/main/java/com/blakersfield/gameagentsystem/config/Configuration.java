34d690af8039f9dbf11292eaab00503f8a91dea6
Blake Pinette <blake.pinette@gmail.com>
2025-04-06 01:04:33 -0700

    missed a file on last commit

---
+++ src/main/java/com/blakersfield/gameagentsystem/config/Configuration.java
@@ -0,0 +1,18 @@
+package com.blakersfield.gameagentsystem.config;
+
+public class Configuration {
+    public static String OLLAMA_BASE_URL = "OLLAMA_BASE_URL";
+    public static String DEFAULT_OLLAMA_BASE_URL = "http://localhost:11434";
+
+    public static String OLLAMA_MODEL = "OLLAMA_MODEL";
+    public static String DEFAULT_OLLAMA_MODEL = "gemma3";
+
+    public static String OPENAI_API_KEY = "OPENAI_API_KEY";
+    public static String DEFAULT_OPENAI_API_KEY = "OPENAI_API_KEY";
+
+    public static String OPENAI_API_SECRET = "OPENAI_API_SECRET";
+    public static String DEFAULT_OPENAI_API_SECRET = "OPENAI_API_SECRET";
+
+    public static String OPENAI_MODEL = "OPENAI_MODEL";
+    public static String DEFAULT_OPENAI_MODEL = "gpt-4";
+}
